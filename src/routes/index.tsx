b563d7a829bdbe554ded47ebaf524567b132c374
Achette <i.achette@live.com>
2022-07-06 17:28:12 -0300

    feat: added ESLint config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@mui/material";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts";
+/* eslint-disable linebreak-style */
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
   const { toggleTheme } = useAppThemeContext();
--- src/shared/contexts/ThemeContext.tsx
+++ src/shared/contexts/ThemeContext.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable linebreak-style */
 import { createContext, useCallback, useMemo, useState, useContext } from 'react';
-import { ThemeProvider } from "@emotion/react";
-import { LightTheme, DarkTheme } from "../themes";
+import { ThemeProvider } from '@emotion/react';
+import { LightTheme, DarkTheme } from '../themes';
 import { Box } from '@mui/system';
 
 
@@ -9,37 +10,38 @@ interface IThemeContextData {
     toggleTheme: () => void}
 
 
-const ThemeContext = createContext({} as IThemeContextData)
+const ThemeContext = createContext({} as IThemeContextData);
 
 interface IThemeProviderProps {
     children: React.ReactNode;
 }
 
 export const useAppThemeContext = () => {
-    return useContext(ThemeContext)
-}
+  return useContext(ThemeContext);
+};
 
+// eslint-disable-next-line react/prop-types
 export const AppThemeProvider: React.FC<IThemeProviderProps> = ({ children }) => {
 
-    const [themeName, setThemeName] = useState<'light' | 'dark'>('light')
+  const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
 
-    const toggleTheme = useCallback((() => {
-        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light')
-    }), [])
+  const toggleTheme = useCallback((() => {
+    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+  }), []);
 
-    const theme = useMemo(() => {
-       if(themeName === 'light') return LightTheme
+  const theme = useMemo(() => {
+    if(themeName === 'light') return LightTheme;
 
-       return DarkTheme
-    }, [themeName])
+    return DarkTheme;
+  }, [themeName]);
 
-    return (
-        <ThemeContext.Provider value={{ themeName, toggleTheme }}>
-             <ThemeProvider theme={theme}>
-                <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
-                {children}
-                </Box>
-             </ThemeProvider>
-        </ThemeContext.Provider>
-    )
-}
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
